672ffe154d3763a5fcabb6f01a035bdd4f5b7e7c
Shawn Jackson <shawn@resgrid.com>
2016-04-03 22:21:21 -0700

    Updated ionic to latest beta, started working on login flow.

--- app/app.ts
+++ app/app.ts
@@ -1,8 +1,6 @@
-import "zone.js";
-import "reflect-metadata";
-import "es6-shim/es6-shim.min";
-
+import "es6-shim";
 import {App, IonicApp, Platform, Events, MenuController} from "ionic-angular";
+import {StatusBar} from "ionic-native";
 import {HomePage} from "./pages/home/home";
 import {WelcomePage} from "./pages/welcome/welcome";
 
@@ -19,7 +17,7 @@ import {LoginService} from "./providers/loginservice";
 class ConnectApp {
   // make HomePage the root (or first) page
   rootPage: any = WelcomePage;
-  pages: Array<{title: string, component: any}>;
+  // pages: Array<{title: string, component: any}>;
 
   constructor(
     private app: IonicApp,
@@ -29,28 +27,17 @@ class ConnectApp {
     this.initializeApp();
 
     // set our app's pages
-    this.pages = [
-      { title: "Home", component: HomePage }//,
-      //{ title: "My First List", component: ListPage }
-    ];
+    // this.pages = [
+    //  { title: "Home", component: HomePage }// ,
+      // { title: "My First List", component: ListPage }
+    // ];
   }
 
   initializeApp() {
     this.platform.ready().then(() => {
-      // The platform is now ready. Note: if this callback fails to fire, follow
-      // the Troubleshooting guide for a number of possible solutions:
-      //
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
-      //
-      // First, let's hide the keyboard accessory bar (only works natively) since
-      // that's a better default:
-      //
-      // Keyboard.setAccessoryBarVisible(false);
-      //
-      // For example, we might change the StatusBar color. This one below is
-      // good for dark backgrounds and light text:
-      // StatusBar.setStyle(StatusBar.LIGHT_CONTENT)
+      // StatusBar.styleDefault();
     });
   }
 }
--- app/pages/login/login.ts
+++ app/pages/login/login.ts
@@ -1,21 +1,26 @@
 import {Page, Events, NavController} from "ionic-angular";
-import {LoginService} from "../../services/loginservice";
-//import {Consts} from "../../consts";
+import {LoginService} from "../../providers/loginservice";
+import {Consts} from "../../consts";
 import {HomePage} from "../home/home";
 
 @Page({
   templateUrl: "build/pages/login/login.html"
 })
 export class LoginPage {
+    userName: string;
+    password: string;
 
-    //constructor(private events: Events, private nav: NavController, private loginService: LoginService) {
-        /* private consts: Consts, */
-        //events.subscribe(consts.Events_UserLoggedIn, (user) => {
-        //    nav.push(HomePage);
-        //});
-     //}
+    constructor(private events: Events, private nav: NavController, private consts: Consts, private loginService: LoginService) {
+        events.subscribe(consts.Events_UserLoggedIn, (user) => {
+            nav.push(HomePage);
+        });
+     }
 
-    doLogin(provider:string){
-      //this.loginService.login(provider);
+    socialLogin(provider:string){
+      this.loginService.login(provider);
+    }
+
+    login() {
+        this.nav.push(HomePage);
     }
 }
